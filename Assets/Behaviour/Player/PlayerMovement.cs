a4125da21308861ebc3e5ad28c05951f14634ec5
FlayerDev <flayer.dev.gr@gmail.com>
2020-09-04 14:40:28 +0300

    basic movement

---
+++ Assets/Behaviour/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 10f;
+
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");      // X- = A ,X+ = D
+        float z = Input.GetAxis("Vertical");        // Z- = S ,Z+ = W
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
