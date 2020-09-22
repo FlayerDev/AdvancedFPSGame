1ebd7641af973dfa630b84503ef4f1a7b07c5d9f
FlayerDev <flayer.dev.gr@gmail.com>
2020-09-22 23:28:59 +0300

    work on Weapon script

--- Assets/Behaviour/Player/Weapon.cs
+++ Assets/Behaviour/Player/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class Weapon : MonoBehaviour
 {
     public bool isWeaponAutomatic = true;
     public GameObject massle;
+    public float baseDamage = 32;
     [Range(10f,2000f)]public float maxDamageDistance = 500;
 
     [SerializeField] bool isArmed = true;
@@ -14,8 +16,6 @@ public class Weapon : MonoBehaviour
     {
         
     }
-
-
     void Update()
     {
         isShooting = Input.GetMouseButton(0);
@@ -26,13 +26,49 @@ public class Weapon : MonoBehaviour
     }
     void fire()
     {
-        Ray ray = new Ray(massle.transform.position, massle.transform.forward);
-        var hitarr = Physics.RaycastAll(ray, maxDamageDistance);
-        sortHitArray(out hitarr);
+        float dmg = baseDamage; GameObject player = null;
+        RaycastHit[] hitarr = Physics.RaycastAll(massle.transform.position, massle.transform.forward, maxDamageDistance);
+        Array.Sort(hitarr, (x, y) => x.distance.CompareTo(y.distance)); // Sorts hit objects by distance
+        if (findPlayer(hitarr, ref player))
+        {
+            foreach (RaycastHit item in hitarr)
+            {
+                if (!item.collider.gameObject.CompareTag("Player")) dmg = calculateDamageDropoff(dmg, item,player);
+                else applyDamage(player);
+            }
+        }
     }
-    void sortHitArray(out RaycastHit[] multiHitInfo)
+    //
+    bool findPlayer(RaycastHit[] arr, ref GameObject player)//Returns the player Game Object, if one is found 
     {
-        multiHitInfo = (Physics.RaycastAll(transform.parent.position, transform.parent.forward, Mathf.Infinity));
-        System.Array.Sort(multiHitInfo, (x, y) => x.distance.CompareTo(y.distance));
+        foreach (var item in arr)
+        {
+            if (item.collider.gameObject.CompareTag("Player"))
+            {
+                player = item.collider.gameObject;
+                return true;
+            }
+        }
+        return false;
     }
+    float calculateDamageDropoff(float dmg, RaycastHit hit, GameObject player)
+    {
+        
+        return dmg;
+    }
+    void applyDamage(GameObject player)
+    {
+        throw new NotImplementedException("Damage Not Applied");
+    } 
+}
+static class DamageDropoffPerMaterial
+{
+    public static Dictionary<string, float> MaterialValue = new Dictionary<string, float>
+    {
+        {"SYNTHETIC", .2f},
+        {"WOOD" , .4f},
+        {"METAL", .8f},
+        {"COBBLE", 1f},
+        {"CONCRETE", 1.5f}
+    };
 }
