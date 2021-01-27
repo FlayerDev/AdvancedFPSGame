58d6b04ec9da7c0b76ddbc74a8db3a7b705be3b3
FlayerDev <flayer.dev.gr@gmail.com>
2021-01-27 23:53:17 +0200

    MinorAnimationChanges

--- Assets/Behaviour/Player/CustomPlayerMovement.cs
+++ Assets/Behaviour/Player/CustomPlayerMovement.cs
@@ -17,14 +17,14 @@ public class CustomPlayerMovement : MonoBehaviour
     public GameObject groundCheckSphere;
     public float AnimationSpeedMultiplier = 1f;
 
-    Vector2 LastLocation = new Vector2(0,0);
+    Vector3 LastLocation = new Vector2(0,0);
     void Update()
     {
         float x = Input.GetAxis("Horizontal");      // X- = A ,X+ = D
         float z = Input.GetAxis("Vertical");        // Z- = S ,Z+ = W
 
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move.normalized * speed * Time.deltaTime);
 
         if (Input.GetKeyDown(LocalInfo.KeyBinds.Jump) && isGrounded && !LocalInfo.IsPaused)
         {
@@ -37,10 +37,17 @@ public class CustomPlayerMovement : MonoBehaviour
     {
         if (LocalInfo.ctrlAnimSpeedSingleton != null)
         {
-            var vec = (new Vector2(gameObject.transform.position.x , gameObject.transform.position.z) - LastLocation) / Time.deltaTime;
-            LocalInfo.ctrlAnimSpeedSingleton.MoveDirection = vec.normalized;
+            var vec = (transform.position - LastLocation) / Time.deltaTime;
+            /*
+            var angle = Vector3.SignedAngle(transform.position, vec, Vector3.up);
+            var movDir = Quaternion.AngleAxis(angle, Vector3.up) * Vector3.zero;
+            Debug.Log(movDir);
+            */
+            LocalInfo.ctrlAnimSpeedSingleton.MoveDirection = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")).normalized; //new Vector2(movDir.x, movDir.z);
+
             LocalInfo.ctrlAnimSpeedSingleton.MoveSpeed = vec.magnitude * AnimationSpeedMultiplier * Time.deltaTime;
-            LastLocation = new Vector2(gameObject.transform.position.x, gameObject.transform.position.z);
+
+            LastLocation = transform.position;
         }
     }
     private void FixedUpdate()
